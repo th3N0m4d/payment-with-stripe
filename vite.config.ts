74e33b5e5cee1849e3d5db88eb7ea2d309a2e7f9
th3N0m4d <edielton.estrela.dantas@sap.com>
2025-08-01 14:31:20 +0200

    feat: add proxy configuration for payment intent in Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/create-payment-intent": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) =>
+          path.replace(/^\/create-payment-intent/, "/create-payment-intent"),
+        // remove any cookie header before it hits your API
+        configure: (proxy) => {
+          proxy.on("proxyReq", (proxyReq) => {
+            proxyReq.removeHeader("cookie");
+          });
+        },
+      },
+    },
+  },
+});
